3939fa6f5d17fff0124dd1c7d7961dcae5ad707b
rhys19 <30848481+rhys19@users.noreply.github.com>
2018-09-18 13:47:57 -0500

    Added Proximity & Added How many players in pursuit

--- client.lua
+++ client.lua
@@ -1,10 +1,10 @@
 --[[
 CREDITS:
 CREATED BY RHYS19
-HALF THE CODE WAS FROM PRIORTY COOLDOWN
+Proximity and players in pursuit by Xander1998
 --]]
 
-isadmin = false
+--[[isadmin = false
 
 RegisterNetEvent('UpdateAdmin')
 AddEventHandler('UpdateAdmin', function(newisadmin)
@@ -14,23 +14,66 @@ end)
 RegisterNetEvent('CancelAdmin')
 AddEventHandler('CancelAdmin', function(newisnotadmin)
     isadmin = newisadmin
-end)
+end)--]]
 
 RegisterNetEvent('AdminScript:NoPerms')
 AddEventHandler('AdminScript:NoPerms', function()
-	TriggerEvent("chatMessage", "Admin Script", {255, 0, 0}, "Insufficient Permissions.")
+	TriggerEvent("chatMessage", "Admin Script", {255, 0, 0}, "Insufficient Permissions to run command.")
 	end)
 
 
+local admins = {}
+
+RegisterNetEvent("adminpursuit:AddAdmin")
+AddEventHandler("adminpursuit:AddAdmin", function(playerID)
+    table.insert(admins, playerID)
+end)
+
+RegisterNetEvent("adminpursuit:RemoveAdmin")
+AddEventHandler("adminpursuit:RemoveAdmin", function(playerID)
+    local index = nil
+    for a = 1, #admins do
+        if admins[a] == playerID then
+            index = a
+        end
+    end
+    if index ~= nil then
+        table.remove(admins, index)
+    end
+end)
+
 Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
-		if isadmin == true then
-			DrawText5("~g~Priorities Are On Hold")
-			DrawText2("~r~ ADMIN PURSUIT NEARBY")
-			DrawText3("~y~ Pull over, and stop!")
-		end
-	end
+    while true do
+        for a = 1, #admins do
+            -- Local Player
+            local lPlayer = PlayerId()
+            local lPlayerPed = GetPlayerPed(lPlayer)
+            local lPlayerPedPos = GetEntityCoords(lPlayerPed, false)
+
+            -- Admin Player
+            local oPlayer = GetPlayerFromServerId(admins[a])
+            local oPlayerPed = GetPlayerPed(oPlayer)
+            local oPlayerPedPos = GetEntityCoords(oPlayerPed, false)
+
+            if GetPlayerServerId(lPlayer) == admins[a] then
+                local distance = Vdist(lPlayerPedPos.x, lPlayerPedPos.y, lPlayerPedPos.z, oPlayerPedPos.x, oPlayerPedPos.y, oPlayerPedPos.z)
+                if distance <= 75.0 then
+                    DrawText5("~g~Priorities Are On Hold")
+                    DrawText2("~r~ ADMIN PURSUIT NEARBY")
+                    DrawText3("~y~ STOP WHAT YOU ARE DOING AND PULL OVER TO THE SIDE OF THE ROAD!")
+
+                    local pursuitCount = 0
+                    for b = 1, #admins do
+                        if admins[a] then
+                            pursuitCount = pursuitCount + 1    
+                        end
+                    end
+                    DrawText4("~o~ There is currently " .. tostring(pursuitCount)  .. " Admin(s) in pursuit!")
+                end
+            end
+        end
+        Citizen.Wait(0)
+    end
 end)
 
 	function DrawText2(text)
