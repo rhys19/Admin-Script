2720e434b08b738a9e8a43c4f697ae129f853a2d
rhys19 <30848481+rhys19@users.noreply.github.com>
2018-10-11 08:10:42 -0500

    Update server.lua
    Added Version Checker

--- server.lua
+++ server.lua
@@ -9,3 +9,30 @@ end, true)
 RegisterCommand("stopadmin", function(source, args, raw)
     TriggerClientEvent("adminpursuit:RemoveAdmin", -1, source)
 end, true)
+
+-- Version Checker do not remove --
+
+local CurrentVersion = '2.0'
+local GithubResourceName = 'Admin-Script'
+
+PerformHttpRequest('https://raw.githubusercontent.com/rhys19/Admin-Script-Stuff/master/VERSION', function(Error, NewestVersion, Header)
+	PerformHttpRequest('https://raw.githubusercontent.com/rhys19/Admin-Script-Stuff/master/CHANGES', function(Error, Changes, Header)
+		print('\n')
+		print('--------------------------------------------------------------------')
+		print('')
+		print('Admin Script')
+		print('')
+		print('Current Version: ' .. CurrentVersion)
+		print('Newest Version: ' .. NewestVersion)
+		io.write("")
+		print(' Changelog: ' .. Changes)
+		print('')
+		if CurrentVersion ~= NewestVersion then
+			print('--------------------------------------------------------------------')
+		else
+			print('-- Up to date!')
+			print('--------------')
+		end
+		print('\n')
+end)
+end)
